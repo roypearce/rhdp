386007033b436f55f13a6640acf2d730ebf6bc53
Roy Pearce <56841696+roypearce@users.noreply.github.com>
2023-05-31 15:52:19 +0000

    Upgrade storybook to 7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,21 @@
 module.exports = {
   stories: ['../.stories/**/*.stories.mdx', '../.stories/**/*.stories.@(tsx)'],
-  addons: [
-    {
-      name: '@storybook/addon-essentials',
-      options: {
-        actions: false,
-        controls: false,
-      },
-    },
-    '@storybook/addon-a11y',
-  ],
-  babel: async (options) => ({
+  addons: [{
+    name: '@storybook/addon-essentials',
+    options: {
+      actions: false,
+      controls: false
+    }
+  }, '@storybook/addon-a11y'],
+  babel: async options => ({
     ...options,
-    plugins: ['istanbul', ...options.plugins],
+    plugins: ['istanbul', ...options.plugins]
   }),
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };
