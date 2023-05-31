064c971abb6faf095b3b885615b45d54bd77f4c5
roypearce <roypearce@users.noreply.github.com>
2023-05-31 23:45:47 +0000

    Automated build updated

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+export * from './chain-date';
+export * from './types';
+export * from './use-datepicker';
+export { isDateValid } from './util';
---
+++ dist/use-datepicker.d.ts
@@ -0,0 +1,2 @@
+import { UseDatepickerProps, UseDatepicker } from './types';
+export declare const useDatepicker: ({ blockedDates, focusOnInit, hasFocusTrap, labels, locale, maxDate, minDate, mode, onChange, onClose, selectDates, weekStart, }: UseDatepickerProps) => UseDatepicker;
